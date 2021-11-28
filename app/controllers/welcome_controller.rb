92108ba2a14aa11d503c27f983e0104c5def97b7
Hisashi Horikawa <hisashi.horikawa@gmail.com>
2021-11-28 20:23:18 +0900

    bit.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,10 @@
+# -*- coding:utf-8 -*-
 
 # OpenID Connect - Relying Party (RP) Sample
 # Copyright (c) Hisashi Horikawa.
 
 class WelcomeController < ApplicationController
+  # ログインページを表示
+  def index
+  end
 end
